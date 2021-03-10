0333b25e7fa87cd7a3006bbc3b46b1790af2a4ac
iamnotstatic <staticdev20046@gmail.com>
2021-03-10 09:56:17 +0100

    updated util file

--- test/utils.js
+++ test/utils.js
@@ -1,15 +1,29 @@
 async function shouldThrow(promise) {
-    try {
-        await promise;
-        assert(true);
+  try {
+    await promise;
+    assert(true);
+  } catch (err) {
+    return;
+  }
+  assert(false, 'The contract did not throw.');
+}
+
+async function expectEvent(promise, event) {
+  try {
+    let result = await promise;
+    for (var i = 0; i < result.logs.length; i++) {
+      var log = result.logs[i];
+      if (log.event === event) {
+        return; // Found the event, return!
+      }
     }
-    catch (err) {
-        return;
-    }
-    assert(false, "The contract did not throw.");
-    
-    }
-    
-    module.exports = {
-        shouldThrow,
-    };
+  } catch (err) {
+    assert(false, 'Exception thrown when not expected');
+  }
+  assert(false, 'Expected ' + event + ' event not fired');
+}
+
+module.exports = {
+  shouldThrow,
+  expectEvent
+};
